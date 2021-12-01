f15ad2e05769ae3f000315ba364014292d7799b9
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-12-01 11:51:58 +0530

    Day 1 completed

---
+++ Day1/first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+
+int main(){
+  std::ifstream fin;
+  fin.open("input.txt");
+  int prev = -1;
+  int current = 0;
+  int ans = 0;
+  while(fin >> current){
+    if (prev != -1 && current > prev){
+      ans++;
+    } 
+    prev = current; 
+  }
+  std::cout << ans << std::endl;
+}
---
+++ Day1/second.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+int main(){
+  std::ifstream fin;
+  fin.open("input.txt");
+  std::vector <int> nums;
+  int current;
+  while(fin >> current){
+    nums.push_back(current);
+  }
+
+  int prev = -1;
+  int ans = 0;
+  for(int i = 0; i < nums.size() - 2; i++){
+    int sum = nums[i] + nums[i + 1] + nums[i + 2];
+    if (prev != -1 && sum > prev){
+      ans++; 
+    }
+    prev = sum;
+  }
+
+  std::cout << ans << std::endl;
+
+
+}
