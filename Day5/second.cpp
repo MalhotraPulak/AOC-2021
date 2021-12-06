226bbbed51b76a92863ce70367562a78d7ee9390
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-12-06 12:05:23 +0530

    Split day 5 into two files

--- Day5/second.cpp
+++ Day5/second.cpp
@@ -8,7 +8,7 @@ int main() {
   fin.open("input.txt");
 
   std::map<std::pair<int, int>, int> m;
-  for(int k = 0; k <  500; k++) {
+  for (int k = 0; k < 500; k++) {
     int x1, y1, x2, y2;
     char comma;
     fin >> x1;
@@ -23,31 +23,42 @@ int main() {
     fin >> comma;
     assert(comma == ',' && "second comma");
     fin >> y2;
-    // std::cout << x1 << y1 << " " << x2 << y2 << std::endl;  
+    // std::cout << x1 << y1 << " " << x2 << y2 << std::endl;
 
-    if (x1 != x2 && y1 != y2)
-      continue;
-     
     int x_min = std::min(x1, x2);
     int x_max = std::max(x1, x2);
     int y_min = std::min(y1, y2);
     int y_max = std::max(y1, y2);
 
-    std::cout << x1 << ":"<< y1 << " " << x2 << ":" << y2 << std::endl;
+    std::cout << x1 << ":" << y1 << " " << x2 << ":" << y2 << std::endl;
     if (x1 == x2) {
       for (int i = y_min; i <= y_max; i++) {
         m[{x1, i}] += 1;
-      } 
-    } else {
+      }
+    } else if (y1 == y2) {
       for (int i = x_min; i <= x_max; i++) {
         m[{i, y1}] += 1;
-      } 
+      }
+    } else {
+      if (x1 > x2) {
+        std::swap(x1, x2);
+        std::swap(y1, y2);
+      }
+      if (y1 < y2) {
+        for (int i = 0; i <= x2 - x1; i++) {
+          m[{x1 + i, y1 + i}] += 1;
+        }
+      } else {
+        for (int i = 0; i <= x2 - x1; i++) {
+          m[{x1 + i, y1 - i}] += 1;
+        }
+      }
     }
 
-    //std::cout << "done" << x1 << " " << y1 << std::endl;
+    // std::cout << "done" << x1 << " " << y1 << std::endl;
   }
   int ans = 0;
-  for (auto &[k, v]: m){
+  for (auto &[k, v] : m) {
     if (v >= 2) {
       ans++;
     }
