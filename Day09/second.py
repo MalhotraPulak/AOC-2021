90f1025b75c61ccb6aed037a327d1c628b7c7a18
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-12-10 17:47:57 +0530

    optimized day 09

--- Day09/second.py
+++ Day09/second.py
@@ -1,89 +1,56 @@
 import sys
-from operator import add
-
-
-dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+import collections
 
 
 def main():
     with open(sys.argv[1], "r") as f:
         lines = f.readlines()
 
-    nums = []
-    for line in lines:
+    grid = collections.defaultdict(lambda: 9)
+
+    rows = len(lines)
+    cols = len(lines[0].strip())
+    for x, line in enumerate(lines):
         line = line.strip()
-        num = []
-        for c in line:
-            num.append(int(c))
-        nums.append(num)
-    rows = len(nums)
-    cols = len(nums[0])
-    dp = {}
+        for y, c in enumerate(line):
+            grid[(x, y)] = int(c)
 
-    def end(p, ending_pts):
-
-        if small_neighbour(p) == 0:
-            ending_pts.append(p)
-            return
-
-        curr_val = nums[p[0]][p[1]]
-
-        for a_dir in dirs:
-            new_pos_x = p[0] + a_dir[0]
-            new_pos_y = p[1] + a_dir[1]
-
-            if (
-                valid((new_pos_x, new_pos_y))
-                and nums[new_pos_x][new_pos_y] < curr_val
-            ):
-                end((new_pos_x, new_pos_y), ending_pts)
-
-    def all_dfs(p):
-        ans = 0
-        for i, line in enumerate(nums):
-            for j, c in enumerate(line):
-                if c == 9:
-                    continue
-                ending_pts = []
-                end((i, j), ending_pts)
-                if ending_pts == [p]:
-                    ans += 1
-        return ans
+    def neigh(x, y):
+        return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
     def valid(p):
-        if 0 <= p[0] < rows and 0 <= p[1] < cols:
-            return True
-        return False
+        return 0 <= p[0] < rows and 0 <= p[1] < cols
 
-    def small_neighbour(p):
-        count = 0
-        for a_dir in dirs:
-            new_pos = (p[0] + a_dir[0], p[1] + a_dir[1])
-            if (
-                valid(new_pos)
-                and nums[new_pos[0]][new_pos[1]] < nums[p[0]][p[1]]
-            ):
-                count += 1
-        return count
+    def get_size(x, y):
+        q = [(x, y)]
+        size = 0
+        visited = set()
+        visited.add(q[0])
+        while len(q) > 0:
+            x, y = q.pop()
+            size += 1
+            for neighbour in neigh(x, y):
+                if (
+                    valid(neighbour)
+                    and grid[neighbour] != 9
+                    and neighbour not in visited
+                ):
+                    q.append(neighbour)
+                    visited.add(neighbour)
+        return size
 
-    ans = []
-    for i, line in enumerate(nums):
-        for j, c in enumerate(line):
-            pos = (i, j)
-            flag = True
-            for a_dir in dirs:
-                new_pos = (pos[0] + a_dir[0], pos[1] + a_dir[1])
-                if valid(new_pos) and nums[new_pos[0]][new_pos[1]] <= c:
-                    flag = False
-                    break
+    lowers = []
+    for i in range(rows):
+        for j in range(cols):
+            val = grid[(i, j)]
+            higher = 0
+            for neighbour in neigh(i, j):
+                higher += 1 if grid[neighbour] > val else 0
+            if higher == 4:
+                lowers.append(get_size(i, j))
 
-            if flag:
-                ans.append(all_dfs(pos))
-                print(
-                    "Got ans =", ans[-1], "for position", pos, "with value", c
-                )
-
-    print(sorted(ans, reverse=True)[:3])
+    lowers.sort()
+    print(lowers[-3] * lowers[-2] * lowers[-1])
 
 
 if __name__ == "__main__":
